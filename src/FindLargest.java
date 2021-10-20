a61b2886cc67dc91d188707336063ca3746f2069
rainaweinberg <raina.weinberg@ssfs.org>
2021-10-20 09:55:37 -0400

    finished findlargest

--- src/FindLargest.java
+++ src/FindLargest.java
@@ -5,6 +5,19 @@ public class FindLargest {
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
+        System.out.println("Enter your first number");
+        int fNum = scan.nextInt();
+        System.out.println("Enter your second number");
+        int sNum = scan.nextInt();
+        if (fNum > sNum) {
+            System.out.print(fNum + " is larger than " + sNum);
+        }
+        if (fNum == sNum) {
+            System.out.print(fNum + " is equal to " + sNum);
+        }
+        if (sNum > fNum) {
+            System.out.print(sNum + " is larger than " + fNum);
+        }
 
         // Prompt the user for two integer variables
 
